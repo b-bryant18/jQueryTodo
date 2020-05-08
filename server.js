8052cdb0385d0834481e00997deb26a7c095d918
Brad Bryant <bradleybryant86@gmail.com>
2020-05-08 01:14:17 -0500

    Added minified CDN for jquery to index.html, created js folder with logic.js file inside it

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+// Dependencies tell node which packages are needed
 const express = require("express");
+const path = require("path");
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.listen(PORT, () => {
+    app.get("/", (req,res) => {
+        res.sendFile(path.join(__dirname, "public/index.html"));
+    })
+console.log("Server started at http://localhost:" + PORT);
+});
+
+// Use nodemon server.js to run a live server that updates itself as changes are saved
 
 
 
