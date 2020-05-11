ae7f67034a94d98193d233bb6ace76727d2b0147
Brad Bryant <bradleybryant86@gmail.com>
2020-05-11 00:34:51 -0500

    Added a note in logic.js about #todolist

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,1 +1,12 @@
-console.log($);
+// #todolist is in index.html
+const todolistUl = $("#todolist");
+
+// data and todo can be called any other name here. They're arbitrary.
+$.get("/api/todos").then(data => {
+    console.log(data);
+    data.forEach(todo => {
+        todolistUl.append(`
+        <li>${todo.text}</li>
+        `)
+    })
+})
--- server.js
+++ server.js
@@ -6,14 +6,31 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+const todos = [
+    {
+        text: "Walk the dog",
+        done: false,
+    },
+    {
+        text: "Wash the car",
+        done: true,
+    }
+
+];
+
 app.listen(PORT, () => {
-    // Use this line to create routes for all files inside the public folder.
+    // Use this line to create routes for all files inside the public folder(top level folder).
     //  No need to make routes for every file
     app.use(express.static("public"))
-    app.get("/", (req,res) => {
+    app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "public/index.html"));
     })
-console.log("Server started at http://localhost:" + PORT);
+
+    app.get("/api/todos", (req, res) => {
+        res.json(todos);
+    })
+
+    console.log("Server started at http://localhost:" + PORT);
 });
 
 // Use nodemon server.js to run a live server that updates itself as changes are saved
