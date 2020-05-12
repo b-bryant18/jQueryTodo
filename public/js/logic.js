213b812cc408a1ec968b803ffa05f46818183afe
Brad Bryant <bradleybryant86@gmail.com>
2020-05-12 00:42:38 -0500

    Created renderTodos function to display new todos as they get saved

--- public/js/logic.js
+++ public/js/logic.js
@@ -3,20 +3,37 @@ const todolistUl = $("#todolist");
 const newTodoInput = $("#new-todo");
 const newTodoButton = $("#save");
 
+// 
+function renderTodos(data) {
+    todolistUl.empty();
+    data.forEach(todo => {
+        todolistUl.append(`
+    <li>${todo.text}</li>
+    `)
+    })
+}
+
 // data and todo can be called any other name here. They're arbitrary.
 $.get("/api/todos").then(data => {
     console.log(data);
-    data.forEach(todo => {
-        todolistUl.append(`
-        <li>${todo.text}</li>
-        `)
-    })
+    renderTodos(data);
 })
 
 // Event handler for save button. e is event
-newTodoButton.click( e => {
-e.preventDefault();
-// What the user wrote in the form
-const userText = newTodoInput.val();
-console.log(userText);
+newTodoButton.click(e => {
+    // Prevents the form from clearing when submitted
+    e.preventDefault();
+    // What the user wrote in the form
+    const userText = newTodoInput.val();
+    console.log(userText);
+    const newTodo = {
+        text: userText,
+        done: false
+    };
+    $.post("/api/todos", newTodo).then(() => {
+        $.get("/api/todos").then(data => {
+            console.log(data);
+            renderTodos(data);
+        })
+    });
 })
